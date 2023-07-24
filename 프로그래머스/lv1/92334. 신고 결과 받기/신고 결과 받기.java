32a4523124c1d2cdd5b61f9e01ed5c21aeaf4fdb
sonhyoseon <80452666+sonhyoseon@users.noreply.github.com>
2023-07-24 21:22:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> map = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
+
+        for (int i = 0; i < id_list.length; i++) {
+            String name = id_list[i];
+            map.put(name, new HashSet<>());
+            idxMap.put(name, i);
+        }
+
+        for (int i = 0; i < report.length; i++) {
+            String[] report_list = report[i].split(" ");
+            map.get(report_list[1]).add(report_list[0]);
+
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            HashSet<String> send = map.get(id_list[i]);
+            if (send.size() >= k) {
+                for (String name : send) {
+                    answer[idxMap.get(name)]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
