a6321ce1ed54f1673751883b60744b3b3167ddb2
sonhyoseon <80452666+sonhyoseon@users.noreply.github.com>
2023-09-09 16:44:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 20.52 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static String solution(String new_id) {
+        String answer = "";
+        new_id = new_id.toLowerCase();
+
+        for (int i = 0; i < new_id.length(); i++) {
+            char ch = new_id.charAt(i);
+            if (Character.isAlphabetic(ch) ||
+                    Character.isDigit(ch) ||
+                    ch == '-' || ch == '_' || ch == '.')
+                answer += ch;
+        }
+
+        while (answer.indexOf("..") != -1)
+            answer = answer.replace("..", ".");
+
+        if (!answer.isEmpty() && answer.charAt(0) == '.')
+            answer = answer.substring(1);
+
+        if (!answer.isEmpty() && answer.charAt(answer.length() - 1) == '.')
+            answer = answer.substring(0, answer.length() - 1);
+
+        if (answer.isEmpty())
+            answer = "a";
+
+        if (answer.length() > 15) {
+            answer = answer.substring(0, 15);
+            if (answer.charAt(answer.length() - 1) == '.')
+                answer = answer.substring(0, answer.length() - 1);
+        }
+
+        // 7
+        while (answer.length() < 3)
+            answer += answer.charAt(answer.length() - 1);
+
+        return answer;
+    }
+
+}
