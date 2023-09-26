c0a5d952757935e0065423d8a3d4d13ce0f85a17
sonhyoseon <80452666+sonhyoseon@users.noreply.github.com>
2023-09-27 00:37:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num) {
+        long number = num;
+        int answer;
+        for(answer = 0; answer < 500; answer++){
+            if(number == 1) return answer;
+            else{
+                if(number % 2 == 0) number /= 2;
+                else number = number * 3 + 1;
+            }
+        } 
+        return -1;
+    }
+}
